8bdff538c43cf2b2bb960241f821e4682edc6dc2
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-10-14 15:23:12 +0200

    Référencement de l'interface

--- app/src/main/java/com/ynov/vernet/pierrefeuilleciseaux/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/pierrefeuilleciseaux/MainActivity.java
@@ -1,14 +1,32 @@
 package com.ynov.vernet.pierrefeuilleciseaux;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView textNbManches, textScoreJ1, textScoreJ2, textStats;
+    Button btnPierre, btnFeuille, btnCiseaux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Référencement de l'interface*/
+
+        // Textes
+        textNbManches = findViewById(R.id.textNbManches);
+        textScoreJ1 = findViewById(R.id.textScoreJ1);
+        textScoreJ2 = findViewById(R.id.textScoreJ2);
+        textStats = findViewById(R.id.textStats);
+
+        // Boutons
+        btnPierre = findViewById(R.id.btnPierre);
+        btnFeuille = findViewById(R.id.btnFeuille);
+        btnCiseaux = findViewById(R.id.btnCiseaux);
     }
 }
